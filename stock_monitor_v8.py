5b0a30676d638c9d65b0ce8744034d3f51be63a8
yulun <manyl1215@gmail.com>
2026-04-23 23:59:50 +0800

    Refactor stock_monitor_v8.py with updated features

--- stock_monitor_v8.py
+++ stock_monitor_v8.py
@@ -63,7 +63,7 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
 
 # 持股清單 (預設範例: 台積電、鴻海、聯發科)
-STOCK_LIST = os.environ.get("STOCK_LIST", "").split(",")
+STOCK_LIST = os.environ.get("STOCK_LIST", "2330,2317,2454,3037,4958,3711,2308,2376,2347,9933").split(",")
 
 # 監控天數 (預設: 15 天)
 MONITOR_DAYS = int(os.environ.get("MONITOR_DAYS", "15"))
